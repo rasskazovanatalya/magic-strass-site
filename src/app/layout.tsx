67771a6162f05263a9ad9ad3e6bce401e8958320
Natalya Rasskazova <rasskazova.natalya.7000948@gmail.com>
2026-05-01 10:14:24 +0300

    add metrika

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,6 +34,31 @@ export default function RootLayout({
           name="yandex-verification"
           content="a7dde6090d5eede5"
         />
+
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(m,e,t,r,i,k,a){
+                  m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                  m[i].l=1*new Date();
+                  for (var j = 0; j < document.scripts.length; j++) {
+                    if (document.scripts[j].src === r) { return; }
+                  }
+                  k=e.createElement(t),a=e.getElementsByTagName(t)[0],
+                  k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+              })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=108996837','ym');
+
+              ym(108996837, 'init', {
+                ssr:true,
+                webvisor:true,
+                clickmap:true,
+                ecommerce:"dataLayer",
+                accurateTrackBounce:true,
+                trackLinks:true
+              });
+            `,
+          }}
+        />
       </head>
       <body className={`${inter.variable} ${prata.variable}`}>
         {children}
