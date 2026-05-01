819c4116c48159e67d5fff6e1ed68c6377888d92
Natalya Rasskazova <rasskazova.natalya.7000948@gmail.com>
2026-05-01 07:13:34 +0300

    yandex verification

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,6 +30,10 @@ export default function RootLayout({
           name="google-site-verification"
           content="eV34j7o0PAlR1or9H09K-_CvXqYT39LjkLAxQ6qmBTM"
         />
+        <meta
+          name="yandex-verification"
+          content="a7dde6090d5eede5"
+        />
       </head>
       <body className={`${inter.variable} ${prata.variable}`}>
         {children}
